8ef9ad48ebd4a1a91b91acc74aec219f1f41fbdf
Mahmoud Abdallah Hassan <Mahmoud-Abdallah@shakeregypt.com>
2025-12-18 16:19:20 +0200

    Initial commit

---
+++ src/day2/ConditionalStatements.java
@@ -0,0 +1,119 @@
+package day2;
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+    static void main(String[] args) {
+        // Example 1
+//        int age = 18;
+//        if(age >=18){
+//            System.out.println("Eligible for vote");
+//        }
+//        else{
+//            System.out.println("Not eligible for vote");
+//        }
+
+        // Example 2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter Number: ");
+//        int number = scanner.nextInt();
+//        if(number%2==0){
+//            System.out.println("Even");
+//        }
+//        else{
+//            System.out.println("Odd");
+//        }
+//        scanner.close();
+
+        // Example 3
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter Number: ");
+//        int number = scanner.nextInt();
+//        if(number>0){
+//            System.out.println("Positive");
+//        }
+//        else if(number<0){
+//            System.out.println("Negative");
+//        }
+//        else{
+//            System.out.println("Zero");
+//        }
+//        scanner.close();
+
+        //Example 4
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter First Number: ");
+//        int num1 = scanner.nextInt();
+//        System.out.println("Enter Second Number: ");
+//        int num2 = scanner.nextInt();
+//        System.out.println("Enter Third Number: ");
+//        int num3 = scanner.nextInt();
+//        if(num1>num2 && num1>num3){
+//            System.out.println("First Number is the Largest");
+//        }
+//        else if(num2>num1 && num2>num3){
+//            System.out.println("Second Number is the Largest");
+//        }
+//        else{
+//            System.out.println("Third Number is the Largest");
+//        }
+//        scanner.close();
+
+        // Example 5
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter Day Number: ");
+//        int dayNumber = scanner.nextInt();
+//        if(dayNumber==1){
+//            System.out.println("Sunday");
+//        }
+//        else if(dayNumber==2){
+//            System.out.println("Monday");
+//        }
+//        else if(dayNumber==3){
+//            System.out.println("Tuesday");
+//        }
+//        else if(dayNumber==4){
+//            System.out.println("Wednesday");
+//        }
+//        else if(dayNumber==5){
+//            System.out.println("Thursday");
+//        }
+//        else if(dayNumber==6){
+//            System.out.println("Friday");
+//        }
+//        else if(dayNumber==7){
+//            System.out.println("Saturday");
+//        }
+//        else{
+//            System.out.println("Invalid Day Number");
+//        }
+
+//        switch(dayNumber){
+//            case 1:
+//                System.out.println("Sunday");
+//                break;
+//            case 2:
+//                System.out.println("Monday");
+//                break;
+//            case 3:
+//                System.out.println("Tuesday");
+//                break;
+//            case 4:
+//                System.out.println("Wednesday");
+//                break;
+//            case 5:
+//                System.out.println("Thursday");
+//                break;
+//            case 6:
+//                System.out.println("Friday");
+//                break;
+//            case 7:
+//                System.out.println("Saturday");
+//                break;
+//            default:
+//                System.out.println("Invalid Day Number");
+//        }
+//        scanner.close();
+    }
+}
+
---
+++ src/day2/LoopingStatements.java
@@ -0,0 +1,30 @@
+package day2;
+
+import java.util.Scanner;
+
+public class LoopingStatements {
+    static void main(String[] args) {
+        int rate = 15;
+        int maxHoursPerWeek = 40;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Working Hours: ");
+        int workingHours = scanner.nextInt();
+
+//        if(workingHours<maxworkingHours){
+//            System.out.println("Your salary is: "+(workingHours*rate)+"$");
+//        }
+//        else{
+//            System.out.println("plese enter valid working hours");
+//            workingHours = scanner.nextInt();
+//        }
+
+        while (workingHours>maxHoursPerWeek) {
+            System.out.println("Please enter number between 1 and 40");
+            workingHours = scanner.nextInt();
+        }
+        System.out.println("Your salary is: "+(workingHours*rate)+"$");
+        scanner.close();
+    }
+}
+
+// you can't use if/else here because you need Loop keeps running while the specified condition remains true
