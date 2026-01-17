273e2b519c6aaa9a1e266259375be39d764835a3
Mahmoud Abdallah <mahmooud117@gmail.com>
2026-01-18 00:57:04 +0200

    Last Update

--- src/day5/MultiDimensionalArray.java
+++ src/day5/MultiDimensionalArray.java
@@ -8,13 +8,29 @@ public class MultiDimensionalArray {
         grades[0][1] = 85;
         grades[0][2] = 70;
         grades[0][3] = 95;
+        grades[1][0] = 65;
+        grades[1][1] = 45;
+        grades[1][2] = 80;
+        grades[1][3] = 75;
+        grades[2][0] = 50;
+        grades[2][1] = 85;
+        grades[2][2] = 90;
+        grades[2][3] = 65;
+        grades[3][0] = 40;
+        grades[3][1] = 85;
+        grades[3][2] = 90;
+        grades[3][3] = 85;
+        grades[4][0] = 90;
+        grades[4][1] = 75;
+        grades[4][2] = 50;
+        grades[4][3] = 85;
 
         System.out.println(grades.length);
         System.out.println(grades[0].length);
 
         System.out.println(grades[0][1]);
 
-        for(int i=0;i< grades.length;i++){
+        for(int i=0;i<grades.length;i++){
             for(int j=0;j<grades[i].length;j++){
                 System.out.print(grades[i][j]+" | ");
             }
