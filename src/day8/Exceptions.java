39bb9e953a1b6d6ae7e3d5b91dd15c5ef2341f3b
Mahmoud Abdallah <mahmooud117@gmail.com>
2026-01-23 15:50:22 +0200

    Last update

--- src/day8/Exceptions.java
+++ src/day8/Exceptions.java
@@ -7,9 +7,10 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exceptions {
-    static void main(String[] args) {
+    static void main(String[] args) throws Exception{
 //        createFile();
-        numbersExceptionHandling();
+        createFileRethrow();
+//        numbersExceptionHandling();
     }
     static void createFile(){
         File file = new File("resources/nonexist.txt");
@@ -23,10 +24,15 @@ public class Exceptions {
         }
         System.out.println("Hello from after exception");
     }
+    static void createFileRethrow() throws IOException{
+        File file = new File("resourcees/nonexist.txt");
+        file.createNewFile();
+    }
     static void numbersExceptionHandling(){
         File file = new File("resources/numbers.txt");
+        Scanner sc = null;
         try{
-            Scanner sc = new Scanner(file);
+            sc = new Scanner(file);
             while(sc.hasNext()){
                 double num = sc.nextDouble();
                 System.out.println(num);
@@ -39,6 +45,9 @@ public class Exceptions {
         catch(InputMismatchException e){
             System.out.println("Mismatching data");
             e.printStackTrace();
+        }
+        finally{
+            sc.close();
         }
         System.out.println("Hello from after exception");
     }
